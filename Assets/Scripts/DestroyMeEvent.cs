91df31684c13c3b29c9ffced94efd0425ca0280a
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-03-25 13:23:01 +0700

    gitignore

--- Assets/Scripts/DestroyMeEvent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DestroyMeEvent : MonoBehaviour
-{
-    public void DestroyMe()
-    {
-        Destroy(gameObject);
-    }
-}
--- Assets/Scripts/RoomController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RoomController : MonoBehaviour
-{
-    public Transform entrance;
-    public Transform exit;
-}
